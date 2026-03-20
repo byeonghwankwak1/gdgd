675037092ab8e7ece20ae6d39bebb1403f1d92b6
곽병환 <jhko369@gmail.com>
2026-03-20 17:42:48 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
 basic.forever(function () {
-	
+    radio.setGroup(1)
 })
